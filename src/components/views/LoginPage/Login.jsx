a59ab81c630a0c780438fdcc8887f514a7ba0f41
sathvikvittal <sathvik.vittal@gmail.com>
2023-04-17 22:02:08 +0530

    login done

--- src/components/views/LoginPage/Login.jsx
+++ src/components/views/LoginPage/Login.jsx
@@ -13,6 +13,8 @@ export default function Login() {
                         <input type="text" placeholder="Username or Email" style={{border:"1px sloid black"}}></input>
                         <br/>
                         <input type="text" placeholder="Password" ></input>
+                        <br/><br/>
+                        <button type="submit" className="submit-button">Sign-in</button>
                     </div>
                 </form>
 
